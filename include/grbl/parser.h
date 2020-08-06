28f42425c27e8cfde909f463ad15ad66a4c41397
Elijah Insua <tmpvar@gmail.com>
2020-08-06 16:02:46 -0700

    fully handle ? response

--- include/grbl/parser.h
+++ include/grbl/parser.h
@@ -15,25 +15,117 @@ enum grbl_response_token_type {
   GRBL_TOKEN_TYPE_STATUS,
   GRBL_TOKEN_TYPE_WELCOME,
   GRBL_TOKEN_TYPE_VERSION,
+  GRBL_TOKEN_TYPE_STATUS_REPORT,
+  GRBL_TOKEN_TYPE_MACHINE_STATE,
+  GRBL_TOKEN_TYPE_MACHINE_POSITION,
+  GRBL_TOKEN_TYPE_WCO_POSITION,
+  GRBL_TOKEN_TYPE_WCO_OFFSET,
+  GRBL_TOKEN_TYPE_BUFFER_STATE,
+  GRBL_TOKEN_TYPE_LINE_NUMBER,
+  GRBL_TOKEN_TYPE_CURRENT_FEED_AND_SPINDLE,
+  GRBL_TOKEN_TYPE_INPUT_PIN_STATE,
+  GRBL_TOKEN_TYPE_OVERRIDES,
+  GRBL_TOKEN_TYPE_ACCESSORY_STATE,
 };
 
-typedef struct grbl_version_t {
+typedef struct grbl_msg_version_t {
   uint16_t major;
   uint16_t minor;
   char letter;
-} grbl_version_t;
+} grbl_msg_version_t;
+
+enum grbl_machine_state {
+  GRBL_MACHINE_STATE_ALARM,
+  GRBL_MACHINE_STATE_CHECK,
+  GRBL_MACHINE_STATE_DOOR,
+  GRBL_MACHINE_STATE_HOLD,
+  GRBL_MACHINE_STATE_HOME,
+  GRBL_MACHINE_STATE_IDLE,
+  GRBL_MACHINE_STATE_JOG,
+  GRBL_MACHINE_STATE_RUN,
+  GRBL_MACHINE_STATE_SLEEP,
+
+  GRBL_MACHINE_STATE_COUNT
+};
+
+const char *grbl_machine_state_str[GRBL_MACHINE_STATE_COUNT] = {
+  "Alarm",
+  "Check",
+  "Door",
+  "Hold",
+  "Home",
+  "Idle",
+  "Jog",
+  "Run",
+  "Sleep"
+};
+
+typedef struct grbl_msg_machine_state_t {
+  grbl_machine_state value;
+  uint16_t code;
+} grbl_msg_status_report_t;
 
 union grbl_response_token_value_t {
   uint8_t error_code;
-  grbl_version_t version;
+  grbl_msg_version_t version;
 };
+
+typedef struct grbl_vec3_t {
+  float x, y, z;
+} grbl_vec3_t;
+
+typedef struct grbl_msg_overrides_t {
+  float feed, rapids, spindle;
+} grbl_msg_overrides_t;
+
+typedef struct grbl_buffer_state_t {
+  uint32_t available_blocks;
+  uint32_t available_bytes;
+} grbl_buffer_state_t;
+
+enum grbl_spindle_direction_state {
+  GRBL_SPINDLE_DIRECTION_OFF,
+  GRBL_SPINDLE_DIRECTION_CW,
+  GRBL_SPINDLE_DIRECTION_CCW,
+};
+
+typedef struct grbl_msg_accessory_state_t {
+  grbl_spindle_direction_state spindle;
+  uint8_t flood_coolant;
+  uint8_t mist_coolant;
+} grbl_msg_accessory_state_t;
+
+typedef struct grbl_msg_current_feed_and_speed_t {
+  uint32_t feed;
+  uint32_t spindle;
+} grbl_msg_current_feed_and_speed_t;
+
+typedef struct grbl_msg_current_pins_t {
+  uint8_t limit_x;
+  uint8_t limit_y;
+  uint8_t limit_z;
+  uint8_t limit_a;
+  uint8_t probe;
+  uint8_t door;
+  uint8_t hold;
+  uint8_t soft_reset;
+  uint8_t cycle_start;
+} grbl_msg_current_pins_t;
 
 typedef struct grbl_response_token_t {
   grbl_response_token_type type;
   //grbl_response_token_value_t value;
   union {
     uint8_t error_code;
-    grbl_version_t version;
+    grbl_msg_version_t version;
+    grbl_msg_machine_state_t machine_state;
+    grbl_vec3_t vec3;
+    grbl_buffer_state_t buffer_state;
+    grbl_msg_current_feed_and_speed_t feed_and_spindle;
+    uint32_t u32;
+    grbl_msg_current_pins_t pins;
+    grbl_msg_overrides_t overrides;
+    grbl_msg_accessory_state_t accessories;
   };
 } grbl_response_token_t;
 
@@ -42,18 +134,6 @@ typedef struct grbl_parser_t {
   grbl_response_token_t *tokens = NULL;
   uint16_t loc=0;
 } grbl_parser_t;
-
-// grbl_response_token_t *grbl_response_token_create(grbl_response_token_type type) {
-//   grbl_response_token_t *token = malloc(sizeof(grbl_response_token));
-//   token->type = type;
-//   return token; 
-// }
-
-// void grbl_response_token_destroy(grbl_response_token_t *token) {
-//   if (token != NULL) {
-//     free(token);
-//   }
-// }
 
 grbl_parser_t *grbl_parser_create() {
   grbl_parser_t *parser = (grbl_parser_t *)malloc(sizeof(grbl_parser_t));
@@ -68,21 +148,31 @@ void grbl_parser_destroy(grbl_parser_t *parser) {
   free(parser);
 }
 
-char *read_int(int *acc, char *buf, uint32_t n) {
+char *advance(char *buf, uint32_t *len, uint32_t n) {
+  if (*len < n) {
+    return NULL;
+  }
+
+  (*len) -= n;
+  return buf + n;
+}
+
+char *read_int(int *acc, char *buf, uint32_t *len) {
   *acc = 0;
   bool negative = false;
   if (buf[0] == '-') {
     negative = true;
+    buf = advance(buf, len, 1);
   }
-
-  for (uint32_t i = 0; buf[0] != 0, i<n; i++) {
-    char c = buf[i];
+  const uint32_t l = (*len);
+  for (uint32_t i = 0; buf[0] != 0, i < l; i++) {
+    char c = buf[0];
     if (c < '0' || c > '9') {
       break;
     }
 
-    (*acc) = ((*acc) * 10) + (buf[i] - '0');
-    buf++;
+    (*acc) = ((*acc) * 10) + (c - '0');
+    buf = advance(buf, len, 1);
   }
 
   if (negative) {
@@ -91,10 +181,10 @@ char *read_int(int *acc, char *buf, uint32_t n) {
   return buf;
 }
 
-char *read_uint16(uint16_t *acc, char *buf, uint32_t n) {
+char *read_uint16(uint16_t *acc, char *buf, uint32_t *len) {
   *acc = 0;
   uint32_t i = 0;
-  for (i;  buf[i] != 0, i<n; i++) {
+  for (i;  buf[i] != 0, i<(*len); i++) {
     char c = buf[i];
     if (c < '0' || c > '9') {
       break;
@@ -103,16 +193,61 @@ char *read_uint16(uint16_t *acc, char *buf, uint32_t n) {
     (*acc) = ((*acc) * 10) + (buf[i] - '0');
   }
 
-  return buf + i;
+  return advance(buf, len, i);
 }
 
-char *advance(char *buf, uint32_t *len, uint32_t n) {
-  if (*len < n) {
-    return NULL;
+char *read_uint32(uint32_t *acc, char *buf, uint32_t *len) {
+  *acc = 0;
+  uint32_t i = 0;
+  for (i;  buf[i] != 0, i<(*len); i++) {
+    char c = buf[i];
+    if (c < '0' || c > '9') {
+      break;
+    }
+
+    (*acc) = ((*acc) * 10) + (buf[i] - '0');
   }
 
-  (*len) -= n;
-  return buf + n;  
+  return advance(buf, len, i);
+}
+
+char *read_float(float *acc, char *buf, uint32_t *len) {
+  (*acc) = 0;
+  int whole = 0;
+  int frac = 0;
+  buf = read_int(&whole, buf, len);
+  if (buf[0] != '.') {
+    (*acc) = (float)whole;
+    return buf;
+  } else {
+    buf = advance(buf, len, 1); // skip the '.'
+  }
+
+  char *new_buf = read_int(&frac, buf, len);
+  size_t frac_len = new_buf - buf;
+
+  (*acc) = (float)whole + (float)frac / pow(10, frac_len);
+
+  return new_buf;
+}
+
+char *read_vec3(grbl_vec3_t *acc, char *buf, uint32_t *len) {
+  // read x
+  buf = read_float(&acc->x, buf, len);
+  if (!buf || buf[0] != ',') {
+    return NULL;
+  }
+  buf = advance(buf, len, 1); // skip ','
+
+  // read y
+  buf = read_float(&acc->y, buf, len);
+  if (!buf || buf[0] != ',') {
+    return NULL;
+  }
+  buf = advance(buf, len, 1); // skip ','
+
+  // read z
+  return read_float(&acc->z, buf, len);
 }
 
 int grbl_parser_tokenize_current_line(grbl_parser_t *parser) {
@@ -147,16 +282,259 @@ int grbl_parser_tokenize_current_line(grbl_parser_t *parser) {
 
     grbl_response_token_t version_token;
     version_token.type = GRBL_TOKEN_TYPE_VERSION;
-    buf = read_uint16(&version_token.version.major, buf, len);
+    buf = read_uint16(&version_token.version.major, buf, &len);
 
+    // skip the '.'
     buf = advance(buf, &len, 1);
 
-    buf = read_uint16(&version_token.version.minor, buf, len);
+    buf = read_uint16(&version_token.version.minor, buf, &len);
     version_token.version.letter = buf[0];
     buf = advance(buf, &len, 1);
 
     sb_push(parser->tokens, version_token);
 
+    return GRBL_TRUE;
+  }
+
+  // handle machine status report
+  if (buf[0] == '<') {
+    {
+      grbl_response_token_t token;
+      token.type = GRBL_TOKEN_TYPE_STATUS_REPORT;
+      sb_push(parser->tokens, token);
+
+      buf = advance(buf, &len, 1);
+    }
+
+    // parse machine status string
+    {
+      grbl_response_token_t token;
+      token.type = GRBL_TOKEN_TYPE_MACHINE_STATE;
+
+      for (size_t i=0; i<GRBL_MACHINE_STATE_COUNT; i++) {
+        const char *state_str = grbl_machine_state_str[i];
+        const size_t l = strlen(state_str);
+
+        if (strstr(buf, state_str) == buf) {
+          buf = advance(buf, &len, l);
+          token.machine_state.value = (grbl_machine_state)i;
+
+          // handle sub codes (currently only Hold:<d> & Door:<d>)
+          if (buf[0] == ':') {
+            buf = advance(buf, &len, 1);
+            buf = read_uint16(&token.machine_state.code, buf, &len);
+          } else {
+            token.machine_state.code = 0;
+          }
+          break;
+        }
+      }
+      sb_push(parser->tokens, token);
+    }
+
+    while (buf[0] != '>' && buf[0] != 0) {
+      // expect all fields to be separated by a |
+      if (buf[0] != '|') {
+        return GRBL_ERROR;
+      }
+      buf = advance(buf, &len,  1);
+
+      grbl_response_token_t token;
+
+      // Machine Position
+      if (strstr(buf, "MPos:") == buf) {
+        buf = advance(buf, &len, 5);
+        buf = read_vec3(&token.vec3, buf, &len);
+        if (buf == NULL) {
+          return GRBL_ERROR;
+        }
+
+        token.type = GRBL_TOKEN_TYPE_MACHINE_POSITION;
+        sb_push(parser->tokens, token);
+        continue;
+      }
+
+      // Work Coordinate Position
+      if (strstr(buf, "WPos:") == buf) {
+        token.type = GRBL_TOKEN_TYPE_WCO_POSITION;
+        buf = advance(buf, &len, 5);
+        buf = read_vec3(&token.vec3, buf, &len);
+        if (buf == NULL) {
+          return GRBL_ERROR;
+        }
+        
+        sb_push(parser->tokens, token);
+        continue;
+      }
+
+      // Work Coordinate Offset
+      if (strstr(buf, "WCO:") == buf) {
+        token.type = GRBL_TOKEN_TYPE_WCO_OFFSET;
+        buf = advance(buf, &len, 4);
+        buf = read_vec3(&token.vec3, buf, &len);
+        if (buf == NULL) {
+          return GRBL_ERROR;
+        }
+
+        sb_push(parser->tokens, token);
+        continue;
+      }
+
+      // Buffer State
+      if (strstr(buf, "Bf:") == buf) {
+        token.type = GRBL_TOKEN_TYPE_BUFFER_STATE;
+        buf = advance(buf, &len, 3);
+        
+        buf = read_uint32(
+          &token.buffer_state.available_blocks,
+          buf,
+          &len
+        );
+
+        if (buf[0] != ',') {
+          return GRBL_ERROR;
+        }
+        buf = advance(buf, &len, 1);
+        if (buf == NULL) {
+          return GRBL_ERROR;
+        }
+
+        buf = read_uint32(
+          &token.buffer_state.available_bytes,
+          buf,
+          &len
+        );
+
+        sb_push(parser->tokens, token);
+        continue;
+      }
+
+      // Line number
+      if (strstr(buf, "Ln:") == buf) {
+        token.type = GRBL_TOKEN_TYPE_LINE_NUMBER;
+        buf = advance(buf, &len, 3);
+        buf = read_uint32(&token.u32, buf, &len);
+        if (buf == NULL) {
+          return GRBL_ERROR;
+        }
+
+        sb_push(parser->tokens, token);
+        continue;
+      }
+
+      // Current Feed (no spindle)
+      if (strstr(buf, "F:") == buf) {
+        token.type = GRBL_TOKEN_TYPE_CURRENT_FEED_AND_SPINDLE;
+        buf = advance(buf, &len, 2);
+        buf = read_uint32(&token.feed_and_spindle.feed, buf, &len);
+        token.feed_and_spindle.spindle = 0;
+        
+        if (buf == NULL) {
+          return GRBL_ERROR;
+        }
+
+        sb_push(parser->tokens, token);
+        continue;
+      }
+
+      // Current Feed and Spindle
+      if (strstr(buf, "FS:") == buf) {
+        token.type = GRBL_TOKEN_TYPE_CURRENT_FEED_AND_SPINDLE;
+        buf = advance(buf, &len, 3);
+        buf = read_uint32(&token.feed_and_spindle.feed, buf, &len);
+
+        if (buf[0] != ',') {
+          return GRBL_ERROR;
+        }
+        buf = advance(buf, &len, 1);
+        if (!buf) {
+          return GRBL_ERROR;
+        }
+
+        buf = read_uint32(&token.feed_and_spindle.spindle, buf, &len);
+
+        if (buf == NULL) {
+          return GRBL_ERROR;
+        }
+
+        sb_push(parser->tokens, token);
+        continue;
+      }
+
+      // Input Pin State
+      if (strstr(buf, "Pn:") == buf) {
+        token.type = GRBL_TOKEN_TYPE_INPUT_PIN_STATE;
+        buf = advance(buf, &len, 3);
+        if (!buf) {
+          return GRBL_ERROR;
+        }
+
+        memset(&token.pins, 0, sizeof(grbl_msg_current_pins_t));
+        while (buf[0] != 0 && buf[0] != '>' && buf[0] != '|') {
+          switch (buf[0]) {
+            case 'X': token.pins.limit_x = 1; break;
+            case 'Y': token.pins.limit_y = 1; break;
+            case 'Z': token.pins.limit_z = 1; break;
+            case 'A': token.pins.limit_a = 1; break;
+            case 'P': token.pins.probe = 1; break;
+            case 'D': token.pins.door = 1; break;
+            case 'H': token.pins.hold = 1; break;
+            case 'R': token.pins.soft_reset = 1; break;
+            case 'S': token.pins.cycle_start = 1; break;
+            default:
+              printf("WARN: unknown pin state %c\n", buf[0]);
+          }
+          buf = advance(buf, &len, 1);
+          if (!buf) {
+            return GRBL_ERROR;
+          }
+        }
+        sb_push(parser->tokens, token);
+        continue;
+      }
+
+      // Overrides
+      if (strstr(buf, "Ov:") == buf) {
+        token.type = GRBL_TOKEN_TYPE_OVERRIDES;
+        buf = advance(buf, &len, 3);
+        grbl_vec3_t v = {0};
+        buf = read_vec3(&v, buf, &len);
+        // turn these into actual percentages
+        token.overrides.feed = v.x / 100.0f;
+        token.overrides.rapids = v.y / 100.0f;
+        token.overrides.spindle = v.z / 100.0f;
+        if (buf == NULL) {
+          return GRBL_ERROR;
+        }
+
+        sb_push(parser->tokens, token);
+        continue;
+      }
+
+      // Accessories
+      if (strstr(buf, "A:") == buf) {
+        token.type = GRBL_TOKEN_TYPE_ACCESSORY_STATE;
+        buf = advance(buf, &len, 2);
+        memset(&token.accessories, 0, sizeof(grbl_msg_accessory_state_t));
+        while (buf[0] != 0 && buf[0] != '>' && buf[0] != '|') {
+          switch (buf[0]) {
+            case 'S': token.accessories.spindle = GRBL_SPINDLE_DIRECTION_CW; break;
+            case 'C': token.accessories.spindle = GRBL_SPINDLE_DIRECTION_CCW; break;
+            case 'F': token.accessories.flood_coolant = 1; break;
+            case 'M': token.accessories.mist_coolant = 1; break;
+            default:
+              printf("WARN: unknown accessory: %c\n", buf[0]);
+          }
+          buf = advance(buf, &len, 1);
+          if (!buf) {
+            return GRBL_ERROR;
+          }
+        }
+        sb_push(parser->tokens, token);
+        continue;
+
+      }
+    }
     return GRBL_TRUE;
   }
 
@@ -172,9 +550,9 @@ int grbl_parser_input(grbl_parser_t *parser, const char c) {
   }
 
   if (c == '\n') {
-    uint16_t len = GRBL_PARSER_BUFFER_LEN-1 > parser->loc
+    uint16_t len = GRBL_PARSER_BUFFER_LEN > parser->loc
     ? parser->loc
-    : GRBL_PARSER_BUFFER_LEN-1;
+    : GRBL_PARSER_BUFFER_LEN;
 
     int ret = GRBL_FALSE;
     if (parser->loc > 0) {
@@ -209,6 +587,16 @@ struct GrblParser {
   }
   int read(const char c) {
     return grbl_parser_input(this->handle, c);
+  }
+
+  int read(const char *str) {
+    const size_t l = strlen(str);
+    for (size_t i=0; i<l; i++) {
+      if (this->read(str[i]) == GRBL_ERROR) {
+        return GRBL_ERROR;
+      }
+    }
+    return GRBL_TRUE;
   }
 
 };
--- include/grbl/test.h
+++ include/grbl/test.h
@@ -1,6 +1,6 @@
 #include "parser.h"
 
-TEST_CASE("basic p tests") {
+TEST_CASE("basic parser tests") {
   GrblParser p;
   // empty lines are ignored
   REQUIRE(p.read('\n') == GRBL_FALSE);
@@ -59,3 +59,133 @@ TEST_CASE("welcome message") {
   REQUIRE(p.handle->tokens[1].version.minor == 2);
   REQUIRE(p.handle->tokens[1].version.letter == 'g');
 }
+
+TEST_CASE("status report (Idle)") {
+  GrblParser p;
+
+  int r = p.read(
+    "<Idle"                     // machine state
+    "|MPos:1.234,5.678,9.012"   // Working coordinate position
+    "|WPos:9.012,5.678,1.234"   // Working coordinate position
+    "|Bf:15,128"                // buffer state
+    "|Ln:99999"                 // line number
+    "|F:500"                    // feed rate (spindle disabled in config.h)
+    "|FS:1000,12000"            // feed + spindle
+    "|WCO:-100.000,0.000,0.000" // working coordinate offset
+    "|Pn:XYZAPDHRS"             // pin inputs
+    "|Ov:123,213,121"           // overrides
+    "|A:SFM"                    // accessory state
+    "|A:C"                      // accessory state
+    ">\r\n"
+  );
+  REQUIRE(r == GRBL_TRUE);
+  REQUIRE(p.handle->tokens[0].type == GRBL_TOKEN_TYPE_STATUS_REPORT);
+  
+  REQUIRE(p.handle->tokens[1].type == GRBL_TOKEN_TYPE_MACHINE_STATE);
+  REQUIRE(p.handle->tokens[1].machine_state.value == GRBL_MACHINE_STATE_IDLE);
+  REQUIRE(p.handle->tokens[1].machine_state.code == 0);
+
+  REQUIRE(p.handle->tokens[2].type == GRBL_TOKEN_TYPE_MACHINE_POSITION);
+  REQUIRE(p.handle->tokens[2].vec3.x == 1.234f);
+  REQUIRE(p.handle->tokens[2].vec3.y == 5.678f);
+  REQUIRE(p.handle->tokens[2].vec3.z == 9.012f);
+
+  REQUIRE(p.handle->tokens[3].type == GRBL_TOKEN_TYPE_WCO_POSITION);
+  REQUIRE(p.handle->tokens[3].vec3.x == 9.012f);
+  REQUIRE(p.handle->tokens[3].vec3.y == 5.678f);
+  REQUIRE(p.handle->tokens[3].vec3.z == 1.234f);
+
+  REQUIRE(p.handle->tokens[4].type == GRBL_TOKEN_TYPE_BUFFER_STATE);
+  REQUIRE(p.handle->tokens[4].buffer_state.available_blocks == 15);
+  REQUIRE(p.handle->tokens[4].buffer_state.available_bytes == 128);
+
+  REQUIRE(p.handle->tokens[5].type == GRBL_TOKEN_TYPE_LINE_NUMBER);
+  REQUIRE(p.handle->tokens[5].u32 == 99999);
+
+  REQUIRE(p.handle->tokens[6].type == GRBL_TOKEN_TYPE_CURRENT_FEED_AND_SPINDLE);
+  REQUIRE(p.handle->tokens[6].feed_and_spindle.feed == 500);
+  REQUIRE(p.handle->tokens[6].feed_and_spindle.spindle == 0);
+
+  REQUIRE(p.handle->tokens[7].type == GRBL_TOKEN_TYPE_CURRENT_FEED_AND_SPINDLE);
+  REQUIRE(p.handle->tokens[7].feed_and_spindle.feed == 1000);
+  REQUIRE(p.handle->tokens[7].feed_and_spindle.spindle == 12000);
+
+  REQUIRE(p.handle->tokens[8].type == GRBL_TOKEN_TYPE_WCO_OFFSET);
+  REQUIRE(p.handle->tokens[8].vec3.x == -100.000f);
+  REQUIRE(p.handle->tokens[8].vec3.y ==  0.000f);
+  REQUIRE(p.handle->tokens[8].vec3.z ==  0.000f);
+
+  REQUIRE(p.handle->tokens[9].type == GRBL_TOKEN_TYPE_INPUT_PIN_STATE);
+  REQUIRE(p.handle->tokens[9].pins.limit_x == 1);
+  REQUIRE(p.handle->tokens[9].pins.limit_y == 1);
+  REQUIRE(p.handle->tokens[9].pins.limit_z == 1);
+  REQUIRE(p.handle->tokens[9].pins.limit_a == 1);
+  REQUIRE(p.handle->tokens[9].pins.probe == 1);
+  REQUIRE(p.handle->tokens[9].pins.door == 1);
+  REQUIRE(p.handle->tokens[9].pins.hold == 1);
+  REQUIRE(p.handle->tokens[9].pins.soft_reset == 1);
+  REQUIRE(p.handle->tokens[9].pins.cycle_start == 1);
+
+  REQUIRE(p.handle->tokens[10].type == GRBL_TOKEN_TYPE_OVERRIDES);
+  REQUIRE(p.handle->tokens[10].overrides.feed == 1.23f);
+  REQUIRE(p.handle->tokens[10].overrides.rapids == 2.13f);
+  REQUIRE(p.handle->tokens[10].overrides.spindle == 1.21f);
+
+  REQUIRE(p.handle->tokens[11].type == GRBL_TOKEN_TYPE_ACCESSORY_STATE);
+  REQUIRE(p.handle->tokens[11].accessories.spindle == GRBL_SPINDLE_DIRECTION_CW);
+  REQUIRE(p.handle->tokens[11].accessories.flood_coolant == 1);
+  REQUIRE(p.handle->tokens[11].accessories.mist_coolant == 1);
+
+  REQUIRE(p.handle->tokens[12].type == GRBL_TOKEN_TYPE_ACCESSORY_STATE);
+  REQUIRE(p.handle->tokens[12].accessories.spindle == GRBL_SPINDLE_DIRECTION_CCW);
+  REQUIRE(p.handle->tokens[12].accessories.flood_coolant == 0);
+  REQUIRE(p.handle->tokens[12].accessories.mist_coolant == 0);
+}
+
+// TEST_CASE("status report (Idle WPos $10=0)") {
+//   GrblParser p;
+
+//   int r = p.read(
+//     "<Idle"                     // machine state
+//     "|WPos:1.234,5.678,9.012"   // Working coordinate position
+//     "|Bf:15,128"                // buffer state
+//     "|Ln:99999"                 // line number
+//     "|F:500"                    // feed rate (spindle disabled in config.h)
+//     "|FS:1000, 12000"           // feed + spindle
+//     "|WCO:-100.000,0.000,0.000" // working coordinate offset
+//     "|Pn:XYZPDHRS"
+//     "|Ov:100,100,100"
+//     ">"
+//   );
+
+//   REQUIRE(r == GRBL_TRUE);
+//   REQUIRE(p.handle->tokens[0].type == GRBL_TOKEN_TYPE_STATUS_REPORT);
+  
+//   REQUIRE(p.handle->tokens[1].type == GRBL_TOKEN_TYPE_MACHINE_STATE);
+//   REQUIRE(p.handle->tokens[1].machine_state.value == GRBL_MACHINE_STATE_IDLE);
+//   REQUIRE(p.handle->tokens[1].machine_state.code == 0);
+
+//   REQUIRE(p.handle->tokens[2].type == GRBL_TOKEN_TYPE_WCO_POSITION);
+//   REQUIRE(p.handle->tokens[2].vec3.x == 1.234f);
+//   REQUIRE(p.handle->tokens[2].vec3.y == 5.678f);
+//   REQUIRE(p.handle->tokens[2].vec3.z == 9.012f);
+// }
+
+// TEST_CASE("status report (Door:1)") {
+//   GrblParser p;
+
+//   int r = p.read("<Door:1|MPos:1.234,5.678,9.012|FS:0,0|WCO:-100.000,0.000,0.000>\r\n");
+//   REQUIRE(r == GRBL_TRUE);
+//   REQUIRE(p.handle->tokens[0].type == GRBL_TOKEN_TYPE_STATUS_REPORT);
+//   REQUIRE(p.handle->tokens[1].type == GRBL_TOKEN_TYPE_MACHINE_STATE);
+//   REQUIRE(p.handle->tokens[1].machine_state.value == GRBL_MACHINE_STATE_DOOR);
+//   REQUIRE(p.handle->tokens[1].machine_state.code == 1);
+
+//   REQUIRE(p.handle->tokens[2].type == GRBL_TOKEN_TYPE_MACHINE_POSITION);
+//   REQUIRE(p.handle->tokens[2].vec3.x == 1.234f);
+//   REQUIRE(p.handle->tokens[2].vec3.y == 5.678f);
+//   REQUIRE(p.handle->tokens[2].vec3.z == 9.012f);
+// }
+
+// <Idle|MPos:0.000,0.000,0.000|FS:0,0|Ov:100,100,100>
+// <Idle|MPos:0.000,0.000,0.000|FS:0,0>
