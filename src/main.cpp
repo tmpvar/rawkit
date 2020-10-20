1999b887271688284c93386f2e223df515dc47ff
Elijah Insua <tmpvar@gmail.com>
2020-10-20 00:14:11 -0700

    [rawkit] hoist imgui basecode so we can modify it freely

--- src/main.cpp
+++ src/main.cpp
@@ -37,7 +37,7 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
 
-#include <imgui/examples/imgui_impl_glfw.h>
+#include "imgui/imgui_impl_glfw.h"
 #include "imgui/imgui_impl_vulkan.h"
 
 #include <rawkit/jit.h>
