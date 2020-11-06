16b2d39a988a1bd5bda339ed4e09da1050c6beb7
Elijah Insua <tmpvar@gmail.com>
2020-11-06 13:39:16 -0700

    [deps/cglm] add header only cglm with SSE and friends disabled

---
+++ include/hot/guest/cglm/cglm.h
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c), Recep Aslantas.
+ *
+ * MIT License (MIT), http://opensource.org/licenses/MIT
+ * Full license can be found in the LICENSE file
+ */
+
+#ifndef cglm_h
+#define cglm_h
+
+// TODO(tmpvar): rawkit-jit doesn't like intrinsics in its current form.
+#undef __SSE2__
+#undef __SSE__
+
+#include "common.h"
+#include "vec2.h"
+#include "vec3.h"
+#include "vec4.h"
+#include "mat4.h"
+#include "mat3.h"
+#include "mat2.h"
+#include "affine.h"
+#include "cam.h"
+#include "frustum.h"
+#include "quat.h"
+#include "euler.h"
+#include "plane.h"
+#include "box.h"
+#include "color.h"
+#include "util.h"
+#include "io.h"
+#include "project.h"
+#include "sphere.h"
+#include "ease.h"
+#include "curve.h"
+#include "bezier.h"
+#include "ray.h"
+#include "affine2d.h"
+
+#endif /* cglm_h */
