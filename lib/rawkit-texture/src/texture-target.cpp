63bbc65f52553784e695b8d9f310571b87e273a6
Elijah Insua <tmpvar@gmail.com>
2021-01-07 15:14:39 -0700

    [rawkit/texture] texture target should store depth when depth is requested

--- lib/rawkit-texture/src/texture-target.cpp
+++ lib/rawkit-texture/src/texture-target.cpp
@@ -207,7 +207,7 @@ rawkit_texture_target_t *rawkit_texture_target_begin(
       attachment.format = get_supported_depth_format(target->gpu->physical_device);
       attachment.samples = VK_SAMPLE_COUNT_1_BIT;
       attachment.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
-      attachment.storeOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+      attachment.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
       attachment.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
       attachment.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
       attachment.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
