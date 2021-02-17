7ae6503e89ec53413542a5608ba5575bb8b7c60d
Elijah Insua <tmpvar@gmail.com>
2021-02-17 14:41:24 -0700

    [brick-engine/xpbd] blob v blob

--- projects/brick-engine/experiments/xpbd.cpp
+++ projects/brick-engine/experiments/xpbd.cpp
@@ -95,12 +95,11 @@ void setup() {
 
 void particle_vs_blob(vec2 &pos, float radius, float inv_mass, Blob *blob, float dt) {
   float d = blob->sample_bilinear_world(pos) - radius;
-  if (d > 0.0) {
+  if (d >= 0.0) {
     return;
   }
 
-  vec2 blob_dir = blob->calc_normal_world(pos);
-  vec2 delta = blob_dir * d;
+  vec2 delta = blob->calc_normal_world(pos) * d;
   vec2 rel = pos - blob->pos;
   float r = angle_between(
     rel,
@@ -120,7 +119,6 @@ void blob_vs_bounds(Blob *blob, vec2 dims) {
   }
 
   u32 c = sb_count(blob->circles);
-  vec2 half = dims * 0.5f;
   for (u32 i=0; i<c; i++) {
     PackedCircle circle = blob->circles[i];
     if (circle.radius < 1.0f || circle.signed_distance >= 0.0) {
@@ -128,7 +126,7 @@ void blob_vs_bounds(Blob *blob, vec2 dims) {
       continue;
     }
     vec2 pos = blob->gridToWorld(circle.pos);
-    float radius = abs(circle.signed_distance);
+    float radius = abs(circle.radius);
     vec2 pos2 = clamp(
       pos,
       vec2(radius),
@@ -153,8 +151,44 @@ void blob_vs_bounds(Blob *blob, vec2 dims) {
     blob->rot += r * 0.5f * inertia;
     blob->pos += delta * (1.0f - inertia);
   }
+}
+
+void blob_vs_blob(Blob *blob0, Blob *blob1) {
+
+  if (!blob0 || !blob0->circles || !blob1) {
+    return;
+  }
+
+  u32 c = sb_count(blob0->circles);
+  for (u32 i=0; i<c; i++) {
+    PackedCircle circle = blob0->circles[i];
+    vec2 pos = blob0->gridToWorld(circle.pos);
+    float radius = abs(circle.signed_distance);
+    float d = blob1->sample_bilinear_world(pos) - radius;
+    if (d >= 0.0) {
+      continue;
+    }
+
+    vec2 delta = blob1->calc_normal_world(pos) * -d;
+    // printf("delta(%f, %f) dist(%f)\n", delta.x, delta.y, d);
+
+    // // TODO: this doesn't take into account the mass ratio along the extent of the shape
+    vec2 rel = pos - blob0->pos;
+    float inertia = distance(blob0->pos, pos) / (length(blob0->dims) * 0.5f);
+    inertia *= 1.5f;
+
+    float r = angle_between(
+      rel,
+      rel + delta
+    );
 
 
+    float w = (blob0->inv_mass) / (blob0->inv_mass + blob1->inv_mass);
+
+    float dt = 1.0/60.0f;
+    blob0->rot += r * inertia * w;
+    blob0->pos += delta * w;
+  }
 }
 
 void loop() {
@@ -165,7 +199,7 @@ void loop() {
   state->mouse.tick();
 
   float now = rawkit_now();
-  float dt = 1.0/60;//now - state->last_time;
+  float dt = now - state->last_time;
   state->last_time = now;
 
   vec2 screen = vec2(
@@ -190,6 +224,7 @@ void loop() {
     u32 c = sb_count(state->blobs);
     for(u32 i=0; i<c; i++) {
       state->blobs[i]->render(ctx);
+      state->blobs[i]->render_circles(ctx);
       igText("blob %s: inertia(%f)", state->blobs[i]->name, state->blobs[i]->inertia);
     }
   }
@@ -223,13 +258,26 @@ void loop() {
       }
     }
 
+    // blob vs blob
+    {
+      u32 c = sb_count(state->blobs);
+      for (u32 i=0; i<c; i++) {
+        for (u32 j=0; j<c; j++) {
+          if (i == j) {
+            continue;
+          }
+          blob_vs_blob(state->blobs[j], state->blobs[i]);
+        }
+      }
+    }
+
     // compute new velocity
     {
       u32 c = sb_count(state->blobs);
       for(u32 i=0; i<c; i++) {
         Blob *blob = state->blobs[i];
         blob->velocity = (blob->pos - state->prev_positions[i]) / substep_dt;
-        blob->angular_velocity = (blob->rot - state->prev_rotations[i]) / substep_dt;
+        blob->angular_velocity = (blob->rot - state->prev_rotations[i]) / substep_dt * 0.99;
       }
     }
   }
