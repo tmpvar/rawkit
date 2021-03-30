a0ae19e8f5299bb34cf39088e745f75b4d31ea9d
Elijah Insua <tmpvar@gmail.com>
2021-03-30 14:38:06 -0700

    [brick-engine/svo/2d] move box center to 0,0

--- projects/brick-engine/svo/2d-traversal.cpp
+++ projects/brick-engine/svo/2d-traversal.cpp
@@ -18,12 +18,19 @@ void setup() {
   if (!state->camera) {
     state->camera = new Camera2D();
   }
-
 }
 
 struct Box {
   vec2 lb;
   vec2 ub;
+  vec2 c_center;
+  Box() {}
+  Box(vec2 lb, vec2 ub) {
+    this->lb = lb;
+    this->ub = ub;
+    this->c_center = (lb + ub) * 0.5f;
+  }
+
   void render(Context2D &ctx, u32 color = 0xFFFFFFFF) {
     vec2 mid = (this->lb + this->ub) * 0.5f;
     ctx.beginPath();
@@ -54,15 +61,14 @@ struct Box {
   }
 
   vec2 center() {
-    return (this->lb + this->ub) * 0.5f;
+    return this->c_center;
   }
 
   u8 quadrant(vec2 p) {
     #if 1
-    vec2 c = (this->lb + this->ub) * 0.5f;
-    float r = distance(c, this->ub);
+    float r = distance(c_center, this->ub);
 
-    bvec2 v = greaterThanEqual(((p - c) / r) + 1.0f, vec2(1.0));
+    bvec2 v = greaterThanEqual(((p - c_center) / r) + 1.0f, vec2(1.0));
     #else
     bvec2 v = greaterThanEqual(
       glm::clamp((p - this->center()), vec2(0.0), vec2(2.0)),
@@ -80,10 +86,7 @@ struct Box {
       (v & 1) > 0 ? r : -r,
       (v & 2) > 0 ? r : -r
     );
-    return {
-      .lb = bc - r,
-      .ub = bc + r
-    };
+    return Box(bc - r, bc + r);
   }
 };
 
@@ -110,27 +113,22 @@ void loop() {
     state->mouse.wheel * 0.1f
   );
 
-  float radius = 0.5f;
-  Box box = {
-    .lb = vec2(0.0),
-    .ub = vec2(2.0)
-  };
+  float radius = 1.0f;
+  Box box(vec2(-1.0), vec2(1.0));
 
   state->camera->begin();
-    ctx.scale(10.0, -10.0);
-    ctx.strokeWidth(0.25 / (state->camera->scale * 10.0));
+    ctx.scale(50.0, -50.0);
+    ctx.strokeWidth(0.75 / (state->camera->scale * 50.0));
     box.render(ctx);
 
     if (state->mouse.button(ImGuiMouseButton_Right)) {
       state->t += 0.01;
     }
 
-    vec2 ro(
-      cos(state->t) * 2.0 + 1.0,
-      sin(state->t) * 2.0 + 1.0
+    vec2 ro = box.center() + vec2(
+      cos(state->t) * 2.0,
+      sin(state->t) * 2.0
     );
-    // vec2 rd(1.0, glm::abs(sin(rawkit_now()) * 2.0));
-
     // vec2 rd = normalize(
     //   state->camera->pointToWorld(
     //     vec2(
@@ -139,89 +137,61 @@ void loop() {
     //     )
     //   ) - ro
     // );
-    vec2 rd = normalize(1.0f - ro);//vec2(1.0, 0.75 + sin(rawkit_now())));
-    // rd = normalize(vec2(1.1, sin(rawkit_now() * 0.1)) - ro);
+    vec2 rd = normalize(box.center() - ro);//vec2(1.0, 0.75 + sin(rawkit_now())));
+
     vec2 ard = abs(rd);
     vec2 aro = ro;
     vec2 ird = 1.0f / ard;
 
+    // The original paper converts all ray directions to negative, but I'll stick with
+    // positive for now.
     u8 quadrant_mask = 0;
     if (rd.x < 0.0) {
       quadrant_mask ^= 1;
       ard.x = -rd.x;
-      // flip the origin over the midpoint (1, 1)
-      if (ro.x > 1.0) {
-        aro.x = 2.0 - ro.x;
-      } else {
-        aro.x = 1.0 - ro.x;
-      }
+      aro.x = -ro.x;
     }
 
     if (rd.y < 0.0) {
       quadrant_mask ^= 2;
       ard.y = -rd.y;
-      // flip the origin over the midpoint (1, 1)
-      if (ro.y > 1.0) {
-        aro.y = 2.0 - ro.y;
-      } else {
-        aro.y = 1.0 - ro.y;
-      }
+      aro.y = -ro.y;
     }
 
-    ard = normalize(ard);
     vec2 aird = 1.0f / ard;
 
     vec2 r = box.isect_ray(aro, ard);
     vec2 start = aro + ard * r.x;
     vec2 end = aro + ard * r.y;
 
+    // render the ray
     {
-      vec2 inv_dir = 1.0f / rd;
-
       ctx.beginPath();
-        ctx.moveTo(ro);
-        ctx.lineTo(ro + rd * 1000.0f);
-        ctx.strokeColor(rgb(0x00FF00FF));
-        ctx.strokeWidth(0.5 / (state->camera->scale * 10.0));
-        ctx.stroke();
-
-      // render sign normalized ray
-      // ctx.beginPath();
-      //   ctx.moveTo(aro);
-      //   ctx.lineTo(aro + ard * 1000.0f);
-      //   ctx.strokeColor(rgb(0x0000FFFF));
-      //   ctx.strokeWidth(0.5 / (state->camera->scale * 10.0));
-      //   ctx.stroke();
+      ctx.moveTo(ro);
+      ctx.lineTo(ro + rd * 1000.0f);
+      ctx.strokeColor(rgb(0x00FF00FF));
+      ctx.stroke();
     }
 
-
-  // iterate
-  #if 1
-    float epsilon = glm::pow(2.0f, -32.0f);
-    igText("r(%f, %f)", r.x, r.y);
-    if (r.x >= r.y) {
-      igText("r.x >= r.y");
-      return;
-    }
-
+    // iterate
     u32 colors[] = { 0xFF0000FF, 0x00FF00FF, 0x0000FFFF };
     u8 quadrant = box.quadrant(start);
 
-    // t.x(x) = ird * x + (-p.x / d.x)
-    // t.y(y) = ird * y + (-p.y / d.y)
+    // setup the ray coefficient:
+    //   given the upper bounds of a node return the x/y crossing
+    //   which can be used
+    // t(upper_corner) = abs_inv_rd * upper_corner + (-p.x / d.x)
     vec2 coef = -start / ard;
 
-    igText("start(%f, %f)", start.x, start.y);
-    igText("ard(%f, %f)", ard.x, ard.y);
+    // define the upper bounds of the lower and upper quadrants
+    vec2 corners(0.0, radius);
 
     for (u32 i=0; i<3; i++) {
-      Box child = box.quadrantToBox(quadrant ^ quadrant_mask);
-      child.render(ctx, colors[i]);
+      box.quadrantToBox(quadrant ^ quadrant_mask).render(ctx, colors[i]);
 
-      // TODO: this needs to be the current scale as we recurse
       vec2 upperCorner(
-        (quadrant & 1<<0) ? 2.0 : 1.0,
-        (quadrant & 1<<1) ? 2.0 : 1.0
+        (quadrant & 1<<0) == 0 ? corners.x : corners.y,
+        (quadrant & 1<<1) == 0 ? corners.x : corners.y
       );
 
       vec2 crossings = aird * upperCorner + coef;
@@ -231,32 +201,5 @@ void loop() {
       }
       quadrant = next_quadrant;
     }
-  #else
-    Box firstBox = box.quadrantToBox(box.quadrant(start));
-    {
-      firstBox.render(ctx, 0xFF0000FF);
-    }
-
-    if (steps > 0) {
-      Box lastBox = box.quadrantToBox(box.quadrant(end));
-      lastBox.render(ctx, 0x00FF00FF);
-    }
-
-    if (steps > 1) {
-      #if 0
-        // brute force computation of the middle quadrant
-        vec2 r1 = firstBox.isect_ray(ro, rd);
-        vec2 r2 = lastBox.isect_ray(ro, rd);
-        vec2 out1 = ro + rd * r1.y;
-        vec2 out2 = ro + rd * r2.x;
-        Box middleBox = box.quadrantToBox(box.quadrant((out1 + out2) * 0.5f));
-      #else
-        // "enlightened" middle node calculation
-        vec2 exits = inv_dir * (box.center() - ro);
-        Box middleBox = box.quadrantToBox(exits.x <= exits.y ? 1 : 2);
-      #endif
-      middleBox.render(ctx, 0x0000FFFF);
-    }
-  #endif
   state->camera->end();
 }
