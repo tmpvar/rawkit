aef80ce97800929aade03daa0b20c640138b2cf8
Elijah Insua <tmpvar@gmail.com>
2021-09-05 15:45:13 -0700

    [rawkit/hot] fix some warnings

--- lib/rawkit-hot/include/rawkit-hot-internal.h
+++ lib/rawkit-hot/include/rawkit-hot-internal.h
@@ -5,12 +5,12 @@
 #include <unordered_map>
 
 
-typedef struct rawkit_hot_entry_t {
+struct rawkit_hot_entry_t {
   const char *name;
   void *data;
   uint64_t len;
 };
 
-typedef struct rawkit_hot_context_t {
+struct rawkit_hot_context_t {
   std::unordered_map<uint64_t, rawkit_hot_entry_t> state_registry;
 };
